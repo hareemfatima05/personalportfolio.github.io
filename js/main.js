8a035f06c24ddbfb289e1e8a93fccb0cc96cbc54
hareemfatima05 <hareemfatima872002@gmail.com>
2023-06-01 21:25:20 +0500

    My Personal Portfolio

---
+++ js/main.js
@@ -0,0 +1,149 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    
+    
+})(jQuery);
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_amcay28";
+    const templateID = "template_aevjbbf";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+
+  // Sticky Navbar
+  let header = document.querySelector('header');
+  let menu = document.querySelector('#menu-icon');
+  let navbar = document.querySelector('.navbar');
+   
+   
+  window.addEventListener('scroll', () => {
+      header.classList.toggle('shadow', window.scrollY > 0);
+  });
+   
+  menu.onclick = () => {
+      navbar.classList.toggle('active');
+  }
+  window.onscroll = () => {
+      navbar.classList.remove('active');
+  }
+   
+//animation 
+  window.addEventListener('DOMContentLoaded', function() {
+      var homeImg = document.querySelector('.home-img');
+      var homeText = document.querySelector('.home-text');
+      var textElements = document.querySelectorAll('.home-text span, .home-text h1, .home-text h2, .home-text p');
+    
+      function animateElements() {
+          homeImg.classList.add('show');
+          homeText.classList.add('show');
+        
+          textElements.forEach(function(element) {
+              element.classList.add('show');
+          });
+      }
+  
+      animateElements();
+  });
+  
+  
+
